5dec08c1aca092f3b8152c6496aa916866dd2b2c
Pranav Garg <pranavgarg@gmail.com>
2018-06-19 12:29:55 +0530

    Created base sctructure to work with Artist class

---
+++ lib/rubyplot/artist.rb
@@ -0,0 +1,11 @@
+module Rubyplot
+  class Artist
+    def initialize
+      @tasks=[]
+    end
+    def write filename
+
+    end
+
+  end
+end
---
+++ lib/rubyplot/bar.rb
@@ -0,0 +1,17 @@
+module Rubyplot
+
+  class Bar < Rubyplot::Artist
+    def title
+
+    end
+    def marker_count
+
+    end
+
+    def data
+      
+    end
+  
+  end
+
+end
