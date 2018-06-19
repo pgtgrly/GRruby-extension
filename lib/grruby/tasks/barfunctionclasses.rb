3aa93f7c5bc08487cce3f36e74b3b5e6dbad5c9c
Pranav Garg <pranavgarg@gmail.com>
2018-06-19 18:04:30 +0530

    Added support for variable number of marker,margin and negative Barsize

--- lib/grruby/tasks/barfunctionclasses.rb
+++ lib/grruby/tasks/barfunctionclasses.rb
@@ -18,26 +18,30 @@ class Draw_bar_class
 end
 
 class Bar_set_axes_class
-  def initialize(xrange,yrange,title)
+  def initialize(xrange,yrange,title,marker_count)
     @xrange = xrange
     @yrange = yrange
     @title = title
-    @titleshift=0
+    @titleshift=0 
+    @marker_count=marker_count
     if @title !=NIL
       @titleshift=0.1
     end
   end
 
   def call()
-    GR.setviewport(0.05, 0.99, 0.05, 0.99-@titleshift)# make it user defined
+    GR.setviewport(0.05, 0.99, 0.05, 0.99-@titleshift)# only decrease the plotting area if title is present
     GR.setwindow(@xrange[0], @xrange[1], @yrange[0], @yrange[1])
     GR.setmarkersize(1.0)#
     GR.setmarkertype(-1)#
     GR.settextalign(2, 0)#
     GR.settextfontprec(103, 0)#
-    GR.setcharheight(0.012)
-    GR.axes(1,(@yrange[1]-@yrange[0])/10,0,0,1,1,0.01)
-    if @titleshift
+    GR.setcharheight(0.012)  
+    if(@marker_count == "default")
+      @marker_count = @yrange[1]-@yrange[0]
+    end
+    GR.axes(1,(@yrange[1]-@yrange[0]).to_f/@marker_count,@xrange[0],@yrange[0],1,1,0.01)
+    if @titleshift!=0
       GR.setcharheight(0.05)
       GR.text(0.5,0.9,@title)
     end
--- lib/rubyplot/bar.rb
+++ lib/rubyplot/bar.rb
@@ -2,19 +2,15 @@
 module Rubyplot
 
   class Bar < Rubyplot::Artist
-    def title
-
-    end
-    def marker_count
-
-    end
+    attr_accessor :margin, :marker_count
+    
 
     def data (dataname,datalist) 
       @dataname = dataname
       @datalist = datalist
       @minval,@maxval= @datalist.minmax
-      @yrange=[@minval-@minval,@maxval+@minval]
-      @xrange=[0,@datalist.size]
+      @margin=1 #decides how many tick above the higest value and below the lowest value
+      @marker_count="default"
     end
 
     def draw_bars # Adds the instance to draw bars to task list
@@ -25,11 +21,13 @@ module Rubyplot
 
     def bar_set_axes #GR framework requires axex to be set up before plotting
 
-      @tasks.push(Bar_set_axes_class.new(@xrange,@yrange,@title))
+      @tasks.push(Bar_set_axes_class.new(@xrange,@yrange,@title,@marker_count))
     end
 
 
     def draw
+      @yrange=[@minval-@margin,@maxval+@margin]
+      @xrange=[0,@datalist.size]
       bar_set_axes
       draw_bars
       super
